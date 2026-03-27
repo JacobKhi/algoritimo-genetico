a59f0acebb52e3a7ef35e39db621b272e9c624fa
Gustavo Jacob <gustavogustavoribeiro2014@gmail.com>
2026-03-27 13:06:13 -0300

    feat: implementa a saida do algoritimo em um arquivo txt

--- src/br/edu/iff/po/genetico/Main.java
+++ src/br/edu/iff/po/genetico/Main.java
@@ -1,5 +1,9 @@
 package br.edu.iff.po.genetico;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /*
     Integrantes:
     Gustavo Jacob Ribeiro, 
@@ -23,27 +27,27 @@ public class Main {
         AlgoritimoGenetico ag = new AlgoritimoGenetico(grafo);
 
         Individuo melhor = ag.evoluir(populacao, 1000, 0.9, 0.02, 2);
-
-        System.out.println("Melhor rota: " + formatarRota(melhor.getRota()));
-        System.out.println("Custo total: " + melhor.getCusto());
-
         Individuo melhorComOpt = ag.doisOpt(melhor);
 
-        System.out.println("\n--- Resultado apos 2-opt ---");
-        System.out.println("Melhor rota: " + formatarRota(melhorComOpt.getRota()));
-        System.out.println("Custo total: " + melhorComOpt.getCusto());
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("saida.txt"));
+            writer.write(melhorComOpt.getCusto() + "\n");
+            writer.write(formatarRota(melhorComOpt.getRota()) + "\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     // Converte os índices internos (começando em 0) para os índices do enunciado (começando em 1)
     // Exemplo: [0, 2, 3, 1] → [1, 3, 4, 2]
     private static String formatarRota(int[] rota) {
-        StringBuilder sb = new StringBuilder("[");
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < rota.length; i++) {
             sb.append(rota[i] + 1);
             if (i < rota.length - 1)
-                sb.append(", ");
+                sb.append(" ");
         }
-        sb.append("]");
         return sb.toString();
     }
 
